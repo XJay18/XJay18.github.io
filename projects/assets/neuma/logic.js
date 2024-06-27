f007bb37eec2cdf3031124acfc1320c0ba3376b3
XJay18 <jaycao2008@126.com>
2024-06-28 02:23:03 +0800

    update June 28

--- projects/assets/neuma/logic.js
+++ projects/assets/neuma/logic.js
@@ -1,12 +1,35 @@
-function showGif(radio) {
+function showGifForRadio(radio) {
     var gifMap = {
-      "jelly": "assets/neuma/kunkun_jelly.gif",
-      "rubber": "assets/neuma/kunkun_rubber.gif",
-      "honey": "assets/neuma/kunkun_honey.gif"
+      "jelly": "assets/neuma/kunkun_jelly",
+      "rubber": "assets/neuma/kunkun_rubber",
+      "honey": "assets/neuma/kunkun_honey"
     };
     var selectedGif = gifMap[radio.value];
     var displayGif = document.getElementById("displayKunkun");
+    var displayView = document.getElementById("demoViewSelection").value;
+    
+    selectedGif = selectedGif + "_view" + displayView + ".gif";
 
     displayGif.src = selectedGif;
     displayGif.style.display = "block";
+}
+
+function showGifForRange(range) {
+  var gifMap = {
+    "jelly": "assets/neuma/kunkun_jelly",
+    "rubber": "assets/neuma/kunkun_rubber",
+    "honey": "assets/neuma/kunkun_honey"
+  };
+  var selected = document.querySelector('input[name="demoNameSelection"]:checked');
+  if (selected) {
+      var selectedGif = gifMap[selected.value];
+      var displayGif = document.getElementById("displayKunkun");
+      var displayView = range.value;
+      
+      selectedGif = selectedGif + "_view" + displayView + ".gif";
+    
+      displayGif.src = selectedGif;
+      displayGif.style.display = "block";
+  }
+
 }
